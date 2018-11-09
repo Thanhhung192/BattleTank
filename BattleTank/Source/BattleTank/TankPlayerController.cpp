8ac94b9ff2829abfb58df38c2f2d3fc42b9434b6
Thanhhung192 <36834849+Thanhhung192@users.noreply.github.com>
2018-11-09 17:58:39 +0700

    Deproject Screen to World

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -17,9 +17,58 @@ void ATankPlayerController::BeginPlay()
 	
 }
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	//Aim toward Cross hair
+	AimTowardCrossHair();
+}
 
 ATank* ATankPlayerController::GetControllerTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardCrossHair()
+{
+	if (!GetControllerTank()){return;}
+	
+	FVector HitLocation; //OUT parameter
+	if (GetSignRayHitLocation(HitLocation))
+	{
+		//UE_LOG(LogTemp, Warning, TEXT("Look Direction %s"), *HitLocation.ToString());
+		
+		//TODO Tell controlled tank to aim at this point
+	}	
+	return;
+}
+
+//GetWorldLocation of line trace through cross hair true if hit landscape
+bool ATankPlayerController::GetSignRayHitLocation(FVector& HitLocation) const
+{
+	//Set cross hair position
+	int32 ViewportSizeX, ViewportSizeY;
+	
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX*CrossHairXLocation, ViewportSizeY*CrossHairYLocation);
+	
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Look Direction %s"), *LookDirection.ToString());
+	}
+		//De project the screen position of the cross hair to a world direction
+			//line trace along that look direction, see what we hit (in a range)
+				//if line trace hit landscape
+					//Get OutHitLocation
+		HitLocation = FVector(1.0);
+	return true;
+}
+
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const
+{
+	FVector WorldLocation;
+	return DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, WorldLocation, LookDirection);
 }
 
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -14,9 +14,25 @@ UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-public:
+
+private:
 	ATank* GetControllerTank() const;
 	
 	 void BeginPlay() override;
-	
+	 virtual void Tick(float DeltaTime) override;
+
+	 //Start the tank moving the barrel so the shot would hit where
+		//the cross hair intersect world
+	 void AimTowardCrossHair();
+	 
+	 bool GetSignRayHitLocation(FVector& HitLocation) const;
+
+	 UPROPERTY(EditAnywhere)
+	 float CrossHairXLocation = 0.5f;
+
+	 UPROPERTY(EditAnywhere)
+	 float CrossHairYLocation = 0.3333333f;
+
+	 bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+	 
 };
